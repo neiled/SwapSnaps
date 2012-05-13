e2a849696ba4f71a6c6530703a8aaecc1e674ddc
Neil Edwards <neil@plasticwater.com>
2012-05-13 22:58:43 +0100

    get port from env if there

--- server.js
+++ server.js
@@ -50,6 +50,9 @@ require('./apps/album/routes')(app)
 // });
 // 
 // 
-app.listen(3000, function(){
+//
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
